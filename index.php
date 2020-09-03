f9e442af5984412bf5356f1f879e986c38742e33
Gideon <2101121@talnet.nl>
2020-09-03 22:50:59 +0200

    herhaling php

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
-//opdracht 1 & 2
+//opdracht 1 & 2 & 3
 
-$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
-    print_r ($klas).PHP_EOL;
+//$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//  foreach ($klas as $i => $value) {
+//      echo "$value". PHP_EOL;
+//  }
+
+$bucket_list = [];
+$hoeveelheid_activiteiten = readline("Hoeveel activiteiten wil je toevoegen aan je bucket list?");
 
 
 
